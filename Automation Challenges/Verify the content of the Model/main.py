38378229878fc850eea0193a3ee0f74f2e72c2a5
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-01-21 13:15:00 +0530

    Update main.py

--- Automation Challenges/Verify the content of the Model/main.py
+++ Automation Challenges/Verify the content of the Model/main.py
@@ -10,16 +10,24 @@ time.sleep(2)
 driver.find_element(By.ID, "showLargeModal").click()
 
 time.sleep(2)
-modal_title = driver.find_element(By.ID, "example-modal-sizes-title-lg").text
+modal_title = driver.find_element(By.XPATH, "(//*[@id='example-modal-sizes-title-lg'])[1]").text
 modal_content = driver.find_element(By.CSS_SELECTOR, ".modal-body").text
 
 expected_title = "Large Modal"
 expected_text_snippet = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 
-assert modal_title == expected_title, f"❌ Title mismatch! Got: {modal_title}"
-assert expected_text_snippet in modal_content, "❌ Modal content does not match!"
+assert modal_title == expected_title, "❌ Modal title mismatch"
+print("✅ Modal title verified")
+
+assert expected_text_snippet in modal_content, "❌ Modal content mismatch"
+print("✅ Modal content verified")
 
 print("✅ Modal content verified successfully!")
+
+# if modal_title == expected_title and expected_text in modal_body:
+#     print("✅ Modal content is correct")
+# else:
+#     print("❌ Modal content is NOT correct")
 
 # Step 5: Close modal
 driver.find_element(By.ID, "closeLargeModal").click()
