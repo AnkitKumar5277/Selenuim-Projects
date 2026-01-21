c2e134815750ed7df87d3c6a3327592d441b8ed8
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-01-21 13:45:16 +0530

    Update web.py

--- Automation Challenges/Web tables verification/web.py
+++ Automation Challenges/Web tables verification/web.py
@@ -6,6 +6,16 @@ driver = webdriver.Edge()
 driver.maximize_window()
 driver.get("https://demoqa.com/webtables")
 
+driver.execute_script("window.scrollBy(0, 200)")
+
+# delete all
+delete1 = driver.find_element(By.XPATH, "(//span[@title='Delete'])[1]")
+delete1.click()
+delete2 = driver.find_element(By.XPATH, "(//span[@title='Delete'])[1]")
+delete2.click()
+delete3 = driver.find_element(By.XPATH, "(//span[@title='Delete'])[1]")
+delete3.click()
+
 # add
 time.sleep(1)
 add = driver.find_element(By.XPATH, "//button[@id='addNewRecordButton']")
@@ -44,8 +54,8 @@ submit_button.click()
 
 # update
 time.sleep(1)
-third_edit_btn = driver.find_element(By.XPATH, "(//span[@title='Edit'])[3]")
-third_edit_btn.click()
+edit_buttons = driver.find_elements(By.XPATH, "//span[@title='Edit']")
+edit_buttons[-1].click()
 
 time.sleep(1)
 update_email = driver.find_element(By.XPATH, "//input[@placeholder='name@example.com']")
@@ -56,16 +66,10 @@ time.sleep(1)
 submit_button = driver.find_element(By.XPATH, "//button[@id='submit']")
 submit_button.click()
 
-# delete (second row for example)
-time.sleep(1)
-delete_2 = driver.find_element(By.XPATH, "(//span[@title='Delete'])[2]")
-delete_2.click()
-
-# delete another (third row for example)
-time.sleep(1)
-delete = driver.find_element(By.XPATH, "(//span[@title='Delete'])[3]")
-delete.click()
+# keep browser open
+time.sleep(9999)
 
 # keep browser open
-time.sleep(100)
-driver.quit()
+while True:
+    pass
+
